0ff299452c36996d15dad72ed380dc2a6374ba83
gavinswilson@gmail.com <gavinswilson@gmail.com>
2024-07-27 16:59:32 -0700

    upd7

--- scsi_read_test.c
+++ scsi_read_test.c
@@ -38,7 +38,7 @@ int main(int argc, char * argv[])
     sg_io_hdr_t io_hdr;
     char * file_name = 0;
     char ebuff[EBUFF_SZ];
-    uint8_t inBuff[_16_REPLY_LEN];
+    char inBuff[_16_REPLY_LEN];
     uint8_t sense_buffer[32];
 
     for (k = 1; k < argc; ++k) {
@@ -120,7 +120,7 @@ int main(int argc, char * argv[])
         char * p = (char *)inBuff;
         for (int i = 0; i < _16_REPLY_LEN-10; i++)
         {
-            //printf("%c", inBuff[i]);
+            printf("%c", inBuff[i]);
             printf("%c ", p[i]);
         }
         
