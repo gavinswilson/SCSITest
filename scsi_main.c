20e3f63e26519a017deb5e2daaacb9c668c42f8a
gavinswilson@gmail.com <gavinswilson@gmail.com>
2024-07-27 20:33:34 -0700

    re-jig

---
+++ scsi_main.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <scsi/sg.h>
+
+
+int main(int argc, char * argv[]) 
+    {
+    return 0
+    }
+
+
+
